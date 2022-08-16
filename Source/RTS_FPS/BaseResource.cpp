35788004d595838d842ab3c569478684a8006356
Gordoco <gordoco416@gmail.com>
2022-08-15 19:04:43 -0500

    UpdatedProceduralResources

--- Source/RTS_FPS/BaseResource.cpp
+++ Source/RTS_FPS/BaseResource.cpp
@@ -18,11 +18,35 @@ ABaseResource::ABaseResource()
 
 void ABaseResource::GenerateMeshes() {
 	MasterMesh->ClearInstances();
-	for (int j = 0; j < NumMeshes; j++) {
-		FVector2D RandomPointInRadius = FMath::RandPointInCircle(SpawningRadius);
-		float x = RandomPointInRadius.X;
-		float y = RandomPointInRadius.Y;
+	FVector2D StartingGridPoint = FVector2D(GetActorLocation().X - SpawningRadius, GetActorLocation().Y + SpawningRadius);
+
+	TArray<FVector2D> GridPoints;
+	int NumPoints = (SpawningRadius*2) / MeshRadialDistance;
+	for (int i = 0; i < NumPoints; i++) {
+		for (int k = 0; k < NumPoints; k++) {
+			FVector2D PotentialPoint = FVector2D(StartingGridPoint.X + (MeshRadialDistance * k), StartingGridPoint.Y + (MeshRadialDistance * i));
+			if (FVector2D::Distance(PotentialPoint, FVector2D(GetActorLocation().X, GetActorLocation().Y)) <= SpawningRadius) {
+				GridPoints.Add(PotentialPoint);
+			}
+		}
+	}
+
+	int limit;
+	if (NumMeshes <= GridPoints.Num()) {
+		limit = NumMeshes;
+	}
+	else {
+		limit = GridPoints.Num();
+	}
+	for (int j = 0; j < limit; j++) {
+
+		int index = FMath::RandRange(0, GridPoints.Num() - 1);
+
+		float x = GridPoints[index].X;
+		float y = GridPoints[index].Y;
 		float z = GetActorLocation().Z;
+
+		GridPoints.RemoveAt(index);
 
 		MasterMesh->AddInstance(FTransform(FVector(x, y, z)));
 	}
